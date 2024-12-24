fc2b27ecdb80b02ce95e4e224a9d1375ba5467ba
pabrul <pablohrbatista@gmail.com>
2024-12-23 21:16:55 -0300

    feat(card): implement reusable KlashaWire card using common card structure

---
+++ src/components/features/dashboard/KlashaWireCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-klasha-primary rounded-lg p-8 text-white flex flex-col h-full">
+    <h3 class="text-2xl font-medium leading-tight mb-6">
+      KlashaWire - send money to businesses globally from Africa
+    </h3>
+
+    <button class="mt-auto bg-black text-white px-6 py-2 rounded-md w-fit">
+      Send a Wire
+    </button>
+  </div>
+</template>
+
+<script setup>
+import Button from "@/components/common/Button.vue";
+</script>
