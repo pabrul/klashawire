606db7a4c36ae6b0264be6e8501b58000f1c7ac0
pabrul <pablohrbatista@gmail.com>
2024-12-23 21:18:42 -0300

    feat(pages): implement Dashboard and Transactions pages with routing

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="p-8">
+    <SalesOverview />
+
+    <div class="mt-12">
+      <SalesFilter />
+
+      <div
+        class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-[1fr,300px] gap-4 sm:gap-6 mt-6"
+      >
+        <MainChart />
+        <KlashaWireCard />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import SalesOverview from "@/components/features/dashboard/SalesOverview.vue";
+import SalesFilter from "@/components/features/dashboard/SalesFilter.vue";
+import MainChart from "@/components/features/dashboard/MainChart.vue";
+import KlashaWireCard from "@/components/features/dashboard/KlashaWireCard.vue";
+</script>
---
+++ src/pages/Transactions.vue
@@ -0,0 +1,7 @@
+<template>
+  <TransactionsTable />
+</template>
+
+<script setup>
+import TransactionsTable from "@/components/features/transactions/TransactionsTable.vue";
+</script>
