c92fd846efdb9e6764b229248aa9ea098fdf4e6b
pabrul <pablohrbatista@gmail.com>
2024-12-23 21:20:16 -0300

    feat(app): configure main application setup

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<!-- src/App.vue -->
+<template>
+  <DefaultLayout>
+    <RouterView />
+  </DefaultLayout>
+</template>
+
+<script setup>
+import DefaultLayout from "../src/layouts/DefaultLayout.vue";
+</script>
